61fb5f28f4e52b3d3e0b99af3c6bf82babcf7ea1
julianpeker@gmail.com <julianpeker@gmail.com@3c5c0bfa-171e-43f5-cb3e-a3ad2b270173>
2013-06-10 14:57:19 +0000

    agregar clases

---
+++ appWeb3/Default2.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NORTHWNDModel;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    NORTHWNDEntities context;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Grilla();
+    }
+    private void Grilla()
+    { 
+        
+        if (context == null) context = new NORTHWNDEntities();
+        GridView1.DataSource = context.Employees;
+        GridView1.DataBind();
+    
+    }
+}
