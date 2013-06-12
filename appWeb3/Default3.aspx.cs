7192a7cbf94f135307c1656923839b965fb4be2b
julianpeker@gmail.com <julianpeker@gmail.com@3c5c0bfa-171e-43f5-cb3e-a3ad2b270173>
2013-06-12 14:02:25 +0000

    ok

---
+++ appWeb3/Default3.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.Dll.Items.Add(new ListItem("Celcius","C"));
+        this.Dll.Items.Add(new ListItem("Fahrenheit", "F"));
+
+    }
+}
