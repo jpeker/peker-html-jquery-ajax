44320278e3ca784596fa9f7ce5fda34b742d5b2e
julianpeker@gmail.com <julianpeker@gmail.com@3c5c0bfa-171e-43f5-cb3e-a3ad2b270173>
2013-06-13 16:03:32 +0000

    suybi esto

---
+++ appWeb3MVC/MvcApplication1/Controllers/VideoPlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class VideoPlayerController : Controller
+    {
+        //
+        // GET: /VideoPlayer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetPlayList()
+        {
+           VideoDbEntities db = new VideoDbEntities();
+
+            var data = db.Video;
+
+            return Json(data.ToArray());
+
+        }
+    }
+}
