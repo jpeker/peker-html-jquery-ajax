051f87a6ac46ca8003dfb3ce446f64da4b7f7061
julianpeker@gmail.com <julianpeker@gmail.com@3c5c0bfa-171e-43f5-cb3e-a3ad2b270173>
2013-06-18 14:48:35 +0000

    mvc4

---
+++ trunk/appWeb4MVC/MvcApplication1/Controllers/ChartController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ChartController : Controller
+    {
+        //
+        // GET: /Chart/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult SaveChart(string data, ChartMaster master, ChartDetail[] details)
+        {
+            Guid id = Guid.NewGuid();
+            string path = HttpContext.Server.MapPath("~/images/" + id.ToString() + ".png");
+            byte[] binaryData = Convert.FromBase64String(data);
+            FileStream file = new FileStream(path, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(file);
+            bw.Write(binaryData);
+            bw.Close();
+            Html5PruebaEntities db = new Html5PruebaEntities();
+            master.Id = id.GetHashCode();
+            master.ImageUrl = "~/images/" + id.ToString() + ".png";
+            db.ChartMaster.Add(master);
+            foreach (ChartDetail detail in details)
+            {
+                detail.ChartId = master.Id;
+                db.ChartDetail.Add(detail);
+            }
+            db.SaveChanges();
+            return Json("Chart saved in the database!");
+        }
+
+    }
+}
